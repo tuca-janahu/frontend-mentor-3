03d6e050cdd41e755da18fb65a224e1e74b02250
tuca-janahu <arturjanahu@gmail.com>
2025-09-03 09:30:37 -0300

    feat: react

---
+++ react-app/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <section className="p-4 mx-auto">
+      <div className="bg-white rounded-lg shadow-md p-6 my-4 mx-auto">
+        <h1 className="text-3xl font-bold">Calculadora de idade</h1>
+      </div>
+    </section>
+  );
+}
+
+export default App
