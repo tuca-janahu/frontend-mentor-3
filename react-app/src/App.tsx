195eaebdc3c23cebcfffccf8385cf0857dcbe771
tuca-janahu <arturjanahu@gmail.com>
2025-09-03 11:53:39 -0300

    feat: form

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,22 @@
-
 function App() {
   return (
-    <section className="p-4 mx-auto">
-      <div className="bg-white rounded-lg shadow-md p-6 my-4 mx-auto">
+    <main className="bg-white max-w-4xl mx-auto md:py-24 md:px-48 py-12 px-10 rounded-t-xl rounded-bl-xl [border-end-end-radius:160px] shadow-lg">
+    <section className=" mx-auto">
         <h1 className="text-3xl font-bold">Calculadora de idade</h1>
-      </div>
+        <form action="">
+            <label htmlFor="day">Dia:</label>
+            <input type="day" id="day" />
+
+            <label htmlFor="month">Mês:</label>
+            <input type="month" id="month" />
+
+            <label htmlFor="year">Ano:</label>
+            <input type="year" id="year" />
+
+            <button className="bg-blue-500 text-white py-2 px-4 rounded" type="submit">Calcular</button>
+        </form>
     </section>
+    </main>
   );
 }
 
